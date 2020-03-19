6f67c2b5641ae1750aaf7c594f79ad27c55f3d6f
aws955 <aws9555@gmail.com>
2020-03-19 11:25:15 +0900

    Finish LearningReactBook

---
+++ LearningReactBook_finish/helloworld/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import HelloWord from './HelloWorld';
+import "./index.css";
+
+ReactDOM.render(
+    <HelloWord/>,
+    document.getElementById('root')
+);
